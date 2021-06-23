9dbe8bf4c812c5cd237ff1bf20c57be6fbf2ab45
Madhu Ayyagari <madhu@Veerashs-MacBook-Air.local>
2021-06-23 11:59:19 -0700

    Updated HTML/CSS/JS

--- app.js
+++ app.js
@@ -4,10 +4,12 @@
 
 
 // Global variables
-const searchSection = document.querySelector('.search-section');
+const searchSection = document.querySelector('#search-section');
 const userInput = document.querySelector('input');
 const searchButton = document.querySelector('#search-button');
-const displayData = document.querySelector('.data-list');
+const displayData = document.querySelector('#data-list');
+const dataContainer = document.querySelector('.data-container')
+
 
 // Adding event listener when user clicks search button
 searchButton.addEventListener('click', search);
@@ -20,35 +22,39 @@ function search() {
 async function displayCurrentData(country) {
   try {
     const response = await axios.get(`https://covid-api.mmediagroup.fr/v1/cases?country=${userInput.value}`)
+    removeOldData(dataContainer);
     const dataList = response.data.All;
     console.log(dataList);
 
     const countryCapital = document.createElement('h2');
     countryCapital.textContent = `Capital: ${dataList.capital_city}`;
-    displayData.append(countryCapital);
+    dataContainer.appendChild(countryCapital);
 
     const confirmCases = document.createElement('h4');
     confirmCases.textContent = `Confirmed Cases: ${dataList.confirmed}`;
-    displayData.append(confirmCases);
+    dataContainer.appendChild(confirmCases);
 
     const recoveredCases = document.createElement('h4');
     recoveredCases.textContent = `Recovered: ${dataList.recovered}`;
-    displayData.append(recoveredCases);
+    dataContainer.appendChild(recoveredCases);
 
     const deathsNums = document.createElement('h4');
     deathsNums.textContent = `Total Deaths: ${dataList.deaths}`;
-    displayData.append(deathsNums);
+    dataContainer.appendChild(deathsNums);
 
     const lastUpdated = document.createElement('h4');
     lastUpdated.textContent = `Last Updated: ${dataList.updated}`;
-    displayData.append(lastUpdated);
-
-
-    
-
+    dataContainer.appendChild(lastUpdated);
 
 
   } catch (error) {
     console.error(error);
+  }
+}
+
+// function to remove last searched data from DOM
+function removeOldData(element) {
+  while (element.lastChild) {
+    element.removeChild(element.lastChild)
   }
 }
